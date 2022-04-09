c5c76bb7af9c9243c9e609106960a3b23b5c840a
Aina Perez Serra <aperezserr@uoc.edu>
2022-04-09 11:16:17 +0200

    final todo app without effects

--- src/app/todos/todo-list-item/todo-list-item.component.ts
+++ src/app/todos/todo-list-item/todo-list-item.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { TodoDTO } from '../models/todo.dto';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { completeTodo, editTodo, deleteTodo } from '../todo.actions'; 
 
 @Component({
   selector: 'app-todo-list-item',
@@ -10,12 +14,38 @@ export class TodoListItemComponent implements OnInit {
 
   @Input() todo: TodoDTO; 
 
-  constructor() {
-    this.todo = new TodoDTO('');
+  titleInput: FormControl;
+  isEditing: boolean;
+
+  constructor(private store: Store<AppState>) {
+   
    }
 
   ngOnInit(): void {
-    
+    this.isEditing = false; 
+    this.titleInput = new FormControl(this.todo.title, Validators.required)  
   }
 
+  completeTask(): void {
+    this.store.dispatch(completeTodo({id: this.todo.id}));
+  }
+
+  editTask(): void {
+    this.isEditing =true;
+    this.titleInput.setValue(this.todo.title);
+  }
+
+  deleteTask(): void {
+    this.store.dispatch(deleteTodo({id: this.todo.id}));
+  }
+
+  submitTask(): void {
+    this.isEditing = false;
+
+    if(!this.titleInput.invalid && this.titleInput.value !== this.todo.title){
+      this.store.dispatch(
+        editTodo({ id: this.todo.id, title: this.titleInput.value})
+      )
+    }
+  }
 }
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -1,13 +1,41 @@
 import { state } from '@angular/animations';
 import { createReducer, on } from '@ngrx/store'; 
 import { TodoDTO } from './models/todo.dto'; 
-import { createTodo } from './todo.actions'; 
+import { completeTodo, createTodo, editTodo, deleteTodo } from './todo.actions'; 
 
 export const initialState: TodoDTO[] = [];
 
 const _todoReducer = createReducer(
     initialState,
-    on(createTodo, (state, {title}) => [...state, new TodoDTO(title)])
+    on(createTodo, (state, {title}) => [...state, new TodoDTO(title)]), 
+    on(completeTodo, (state, { id }) => {
+        return state.map((todo) => {
+           if(todo.id === id) {
+            console.log('changing')
+            return {
+                ...todo,
+                done: true,
+                }
+            }else{
+                return todo;
+            } 
+        })
+    }), 
+    on(editTodo, (state, {id, title }) => {
+        return state.map((todo)=>{
+            if(todo.id === id) {
+                return{
+                    ...todo, 
+                    title: title,
+                }
+            }else{
+                return todo;
+            }
+        });
+    }), 
+    on(deleteTodo, (state, {id})=>{
+        return state.filter(todo => todo.id !== id)
+    })
 );
 
 export function todoReducer(state: any, action: any) {
